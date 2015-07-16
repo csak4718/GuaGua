3fd9487baf4c01c5e76c03b22f376dbb666706b1
FatMinMin <chiehmin18@gmail.com>
2015-07-17 00:42:02 +0800

    add swipeable card support

--- app/src/main/java/com/yahoo/mobile/itern/guagua/Adapter/QuestionCardAdapter.java
+++ app/src/main/java/com/yahoo/mobile/itern/guagua/Adapter/QuestionCardAdapter.java
@@ -2,13 +2,18 @@ package com.yahoo.mobile.itern.guagua.Adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.h6ah4i.android.widget.advrecyclerview.swipeable.RecyclerViewSwipeManager;
+import com.h6ah4i.android.widget.advrecyclerview.swipeable.SwipeableItemAdapter;
+import com.h6ah4i.android.widget.advrecyclerview.utils.AbstractSwipeableItemViewHolder;
 import com.parse.ParseObject;
 import com.yahoo.mobile.itern.guagua.R;
 
@@ -17,7 +22,8 @@ import java.util.List;
 /**
  * Created by cmwang on 7/16/15.
  */
-public class QuestionCardAdapter extends RecyclerView.Adapter<QuestionCardAdapter.ViewHolder> {
+public class QuestionCardAdapter extends RecyclerView.Adapter<QuestionCardAdapter.ViewHolder>
+        implements SwipeableItemAdapter<QuestionCardAdapter.ViewHolder> {
 
 
     private List<ParseObject> mQuestionList;
@@ -29,8 +35,9 @@ public class QuestionCardAdapter extends RecyclerView.Adapter<QuestionCardAdapte
         mContext = context;
         mQuestionList = list;
         mInflater = LayoutInflater.from(context);
+        setHasStableIds(true);
     }
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends AbstractSwipeableItemViewHolder {
         public View mView;
         public TextView txtTitle;
         public Button btnA;
@@ -42,6 +49,11 @@ public class QuestionCardAdapter extends RecyclerView.Adapter<QuestionCardAdapte
             btnA = (Button) v.findViewById(R.id.btnA);
             btnB = (Button) v.findViewById(R.id.btnB);
         }
+        @Override
+        public View getSwipeableContainerView() {
+            return mView;
+        }
+
     }
 
     @Override
@@ -87,7 +99,30 @@ public class QuestionCardAdapter extends RecyclerView.Adapter<QuestionCardAdapte
     }
 
     @Override
+    public long getItemId(int position) {
+        return mQuestionList.get(position).hashCode();
+    }
+
+    @Override
     public int getItemCount() {
         return mQuestionList.size();
+    }
+
+    @Override
+    public int onGetSwipeReactionType(ViewHolder holder, int position, int x, int y) {
+        return RecyclerViewSwipeManager.REACTION_CAN_SWIPE_BOTH;
+    }
+
+    @Override
+    public void onSetSwipeBackground(ViewHolder holder, int position, int type) {
+    }
+
+    @Override
+    public int onSwipeItem(ViewHolder holder, int position, int result) {
+        return RecyclerViewSwipeManager.AFTER_SWIPE_REACTION_DEFAULT;
+    }
+
+    @Override
+    public void onPerformAfterSwipeReaction(ViewHolder holder, int position, int result, int reaction) {
     }
 }
--- app/src/main/java/com/yahoo/mobile/itern/guagua/Fragment/MainActivityFragment.java
+++ app/src/main/java/com/yahoo/mobile/itern/guagua/Fragment/MainActivityFragment.java
@@ -14,6 +14,10 @@ import android.widget.Button;
 import android.widget.ListView;
 
 import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.h6ah4i.android.widget.advrecyclerview.animator.GeneralItemAnimator;
+import com.h6ah4i.android.widget.advrecyclerview.animator.SwipeDismissItemAnimator;
+import com.h6ah4i.android.widget.advrecyclerview.swipeable.RecyclerViewSwipeManager;
+import com.h6ah4i.android.widget.advrecyclerview.touchguard.RecyclerViewTouchActionGuardManager;
 import com.parse.ParseObject;
 import com.yahoo.mobile.itern.guagua.Adapter.QuestionCardAdapter;
 import com.yahoo.mobile.itern.guagua.Event.QuestionEvent;
@@ -21,6 +25,7 @@ import com.yahoo.mobile.itern.guagua.R;
 import com.yahoo.mobile.itern.guagua.Util.ParseUtils;
 import com.yalantis.phoenix.PullToRefreshView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import de.greenrobot.event.EventBus;
@@ -37,6 +42,10 @@ public class MainActivityFragment extends Fragment {
     private RecyclerView.LayoutManager mLayoutManager;
     private PullToRefreshView mPullToRefreshView;
     private QuestionCardAdapter mAdapter;
+    private RecyclerView.Adapter mWrappedAdapter;
+    private RecyclerViewSwipeManager mRecyclerViewSwipeManager;
+    private RecyclerViewTouchActionGuardManager mRecyclerViewTouchActionGuardManager;
+    private List<ParseObject> mList;
 
     public MainActivityFragment() {
     }
@@ -59,8 +68,9 @@ public class MainActivityFragment extends Fragment {
     }
 
     private void refreshList(List<ParseObject> list) {
-        mAdapter = new QuestionCardAdapter(getActivity(), list);
-        mRecyclerView.setAdapter(mAdapter);
+        mList.clear();
+        mList.addAll(list);
+        mWrappedAdapter.notifyDataSetChanged();
         mPullToRefreshView.setRefreshing(false);
     }
 
@@ -68,11 +78,45 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.fragment_main, container, false);
-        mBtnAddPost = (FloatingActionButton) mView.findViewById(R.id.btn_add_post);
-        mRecyclerView = (RecyclerView) mView.findViewById(R.id.recycler_view);
-        mLayoutManager = new LinearLayoutManager(getActivity());
-        mRecyclerView.setLayoutManager(mLayoutManager);
+        return mView;
+    }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        mBtnAddPost = (FloatingActionButton) mView.findViewById(R.id.btn_add_post);
+
+
+        mRecyclerView = (RecyclerView) getView().findViewById(R.id.recycler_view);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+
+        // touch guard manager  (this class is required to suppress scrolling while swipe-dismiss animation is running)
+        mRecyclerViewTouchActionGuardManager = new RecyclerViewTouchActionGuardManager();
+        mRecyclerViewTouchActionGuardManager.setInterceptVerticalScrollingWhileAnimationRunning(true);
+        mRecyclerViewTouchActionGuardManager.setEnabled(true);
+
+        // swipe manager
+        mRecyclerViewSwipeManager = new RecyclerViewSwipeManager();
+
+        mList = new ArrayList<>();
+        mAdapter = new QuestionCardAdapter(getActivity(), mList);
+
+        mWrappedAdapter = mRecyclerViewSwipeManager.createWrappedAdapter(mAdapter);      // wrap for swiping
+
+        final GeneralItemAnimator animator = new SwipeDismissItemAnimator();
+
+        // Change animations are enabled by default since support-v7-recyclerview v22.
+        // Disable the change animation in order to make turning back animation of swiped item works properly.
+        animator.setSupportsChangeAnimations(false);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mWrappedAdapter);  // requires *wrapped* adapter
+        mRecyclerView.setItemAnimator(animator);
+
+        mRecyclerViewTouchActionGuardManager.attachRecyclerView(mRecyclerView);
+        mRecyclerViewSwipeManager.attachRecyclerView(mRecyclerView);
+
+        // set up pull to refresh
         mPullToRefreshView = (PullToRefreshView) mView.findViewById(R.id.pull_to_refresh);
         mPullToRefreshView.setOnRefreshListener(new PullToRefreshView.OnRefreshListener() {
             @Override
@@ -96,8 +140,5 @@ public class MainActivityFragment extends Fragment {
         });
 
         ParseUtils.getAllQuestions();
-
-
-        return mView;
     }
 }
