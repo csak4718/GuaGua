2d59ace130dd4da6f3b0cbbf7a502f88ae50e565
FatMinMin <chiehmin18@gmail.com>
2015-07-22 12:14:33 +0800

    add fb login function

--- app/src/main/java/com/yahoo/mobile/itern/guagua/Activity/LoginActivity.java
+++ app/src/main/java/com/yahoo/mobile/itern/guagua/Activity/LoginActivity.java
@@ -1,20 +1,62 @@
 package com.yahoo.mobile.itern.guagua.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import com.parse.LogInCallback;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
 import com.yahoo.mobile.itern.guagua.R;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LoginActivity extends ActionBarActivity {
+
+    Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mLoginButton = (Button) findViewById(R.id.btn_login_facebook);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final List<String> permissions = new ArrayList<>();
+                permissions.add("public_profile");
+                permissions.add("user_status");
+                permissions.add("user_friends");
+
+                ParseFacebookUtils.logInWithReadPermissionsInBackground(LoginActivity.this, permissions, new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, com.parse.ParseException e) {
+                        if (user == null) {
+                            Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
+                        } else if (user.isNew()) {
+                            Log.d("MyApp", "User signed up and logged in through Facebook!");
+                        } else {
+                            Log.d("MyApp", "User logged in through Facebook!");
+                        }
+                    }
+                });
+            }
+        });
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/com/yahoo/mobile/itern/guagua/Application/MainApplication.java
+++ app/src/main/java/com/yahoo/mobile/itern/guagua/Application/MainApplication.java
@@ -2,7 +2,9 @@ package com.yahoo.mobile.itern.guagua.Application;
 
 import android.app.Application;
 
+import com.facebook.FacebookSdk;
 import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
 
 
 /**
@@ -11,9 +13,10 @@ import com.parse.Parse;
 public class MainApplication extends Application {
     @Override
     public void onCreate() {
-
+        FacebookSdk.sdkInitialize(getApplicationContext());
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "iMyUdfPQnXeU1bTHi3f8jhRw5oCx40UxvMfcicno", "fwtpApBFDvfTtUHJ5nwrdqD8y5lVoU3nePIQmW6k");
+        ParseFacebookUtils.initialize(this);
         super.onCreate();
     }
 }
