6f7cfb6cf057745bd9a71d0f3e6d62bf6c6492bb
FatMinMin <chiehmin18@gmail.com>
2015-08-07 15:03:47 +0800

    add push notification library

--- app/src/main/java/com/yahoo/mobile/itern/guagua/Application/MainApplication.java
+++ app/src/main/java/com/yahoo/mobile/itern/guagua/Application/MainApplication.java
@@ -1,12 +1,16 @@
 package com.yahoo.mobile.itern.guagua.Application;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.facebook.FacebookSdk;
 import com.flurry.android.FlurryAgent;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseFacebookUtils;
 import com.parse.ParseObject;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 
 
 /**
@@ -22,6 +26,19 @@ public class MainApplication extends Application {
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "iMyUdfPQnXeU1bTHi3f8jhRw5oCx40UxvMfcicno", "fwtpApBFDvfTtUHJ5nwrdqD8y5lVoU3nePIQmW6k");
         ParseFacebookUtils.initialize(this);
+
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
+
+
 
         FlurryAgent.init(this, "G4GPJ92FFBWHGZCH8WCK");
         super.onCreate();
